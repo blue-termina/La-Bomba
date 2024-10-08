affd11ea18275f698a9ab1dfe6d707656bb2000d
blue-terminal <marco.dandrea777@gmail.com>
2024-10-08 11:21:30 +0200

    Delete MyClass.java

--- MyClass.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class MyClass {
-    public static void main(String args[]) {
-    Scanner Scanner =new Scanner(System.in);
-    System.out.print("inserire età\n");
-    int età =Scanner.nextInt();
-    System.out.print(età);
-    }
-}
